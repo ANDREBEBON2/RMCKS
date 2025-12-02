5c006bbe32879675f12fb21579f66d98d9e9d099
ANDREBEBON2 <andrebebon21@gmail.com>
2025-12-02 15:08:56 +0800

    menambah fungsi di class login

--- app/Livewire/Guest/Login.php
+++ app/Livewire/Guest/Login.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Guest;
 
-use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\Auth;
 
 class Login extends Component
 {
     #[Layout('components.layouts.guest')]
+
+
+    public $email = '';
+    public $password = '';
+
+    public function masuk(){
+        $this->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt(['email' => $this->email, 'password' => $this->password])){
+            session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        $this->addError('email', 'username atau password salah.');
+
+
+    }
+
     public function render()
     {
         return view('livewire.guest.login');
