5e5e32d8736e3a3d190f76c695c3ef15433eefcc
ANDREBEBON2 <andrebebon21@gmail.com>
2025-12-05 18:24:13 +0800

    menambah icon edit dan delete

--- resources/views/livewire/auth/user.blade.php
+++ resources/views/livewire/auth/user.blade.php
@@ -5,7 +5,7 @@
 
     {{-- button --}}
     <div class="py-2">
-        <button class="bg-primary text-accent px-3 py-2 flex items-center gap-1 rounded text-sm">
+        <button class="bg-primary text-accent px-3 py-2 flex items-center gap-1 rounded text-sm hover:scale-95 transform transition-all duration-200">
             <x-icons.add class="size-3"/>
             <span>Add User</span>
         </button>
@@ -29,6 +29,9 @@
                 <th scope="col" class="py-3 px-6">
                     Role
                 </th>
+                <th scope="col" class="py-3 px-6">
+                    Action
+                </th>
             </tr>
         </thead>
 
@@ -48,6 +51,14 @@
                         User
                     </span>
                 </td>
+                <td class="py-4 px-6 space-x-2 flex">
+                   <button class="h-8 w-8 bg-primary text-accent rounded flex items-center justify-center hover:scale-95 transform transition-all duration-200">
+                    <x-icons.edit class="size-5"/>
+                   </button>
+                   <button class="h-8 w-8 bg-primary text-accent rounded flex items-center justify-center hover:scale-95 transform transition-all duration-200 group">
+                    <x-icons.delete class="size-5"/>
+                   </button>
+                </td>
             </tr>
             <tr class="bg-white border-b hover:bg-gray-50 transition duration-200">
                 <td class="py-4 px-6 text-center">
@@ -63,6 +74,14 @@
                     <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
                         Admin
                     </span>
+                </td>
+                <td class="py-4 px-6 space-x-2 flex">
+                   <button class="h-8 w-8 bg-primary text-accent rounded flex items-center justify-center hover:scale-95 transform transition-all duration-200">
+                    <x-icons.edit class="size-5"/>
+                   </button>
+                   <button class="h-8 w-8 bg-primary text-accent rounded flex items-center justify-center hover:scale-95 transform transition-all duration-200">
+                    <x-icons.delete class="size-5"/>
+                   </button>
                 </td>
             </tr>
         </tbody>
